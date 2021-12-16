15d9cba95b4ca46f2150182b43ed5b78f0a2e753
Yesica Colman <yesica.colman@gmail.com>
2021-12-16 20:49:31 -0300

    Arreglo el error de mergeo.

--- PreEntregaCoder/AppCoder/views.py
+++ PreEntregaCoder/AppCoder/views.py
@@ -7,13 +7,7 @@ def inicio(request):
 
     return render(request, 'AppCoder/inicio.html')
 
-<<<<<<< HEAD
+
 def productos(request):
 
-    return render(request, 'AppCoder/productos.html')
-=======
-
-def Productos(request):
-
     return render(request, 'AppCoder/Productos.html')
->>>>>>> e59efd4366abba8b1c91e9a6199c475f84f6c502
